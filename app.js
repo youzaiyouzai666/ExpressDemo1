ebb7295d1cdef3c3f89479829cb3942bf36394ae
youzaiyouzai666 <1620030379@qq.com>
2016-11-12 20:20:37 +0800

    init

--- app.js
+++ app.js
@@ -4,15 +4,18 @@
 var express = require('express');
 var app = express();
 
-// set up handlebars view engine
+/*
+ * set up handlebars view engine
+ * layout目录与视图目录的配置？
+ */
 var handlebars = require('express3-handlebars')
     .create({ defaultLayout:'main' });
-app.engine('handlebars', handlebars.engine);
-app.set('view engine', 'handlebars');
+    app.engine('handlebars', handlebars.engine);
+    app.set('view engine', 'handlebars');
 
-//set 'showTests' context property if the querystring contains test=1
+//set 'showTests' context propderty if the querystring contains test=1
 app.use(function(req, res, next){
-    res.locals.showTests = req.query.test == '1';
+    res.locals.showTests = req.query.test == '1';//'showTests' make use of View
     next();//Notice: don't forget
 });
 
--- test/test.js
+++ test/test.js
@@ -6,3 +6,17 @@ console.log(module.exports);
 exports= function(){
     console.log('dsfa');
 };
+ //执行 node E:\node_study\file_path_study\app.js
+
+ console.log('*** app start ***');
+
+ console.log('***      module.filename = ' + module.filename + ' ***');
+ console.log('***           __filename = ' + __filename + ' ***');
+ console.log('***            __dirname = ' + __dirname + ' ***');
+ console.log('***        process.cwd() = ' + process.cwd() + ' ***');
+console.log('*** require.main.filename= ' + require.main.filename + ' ***');
+
+ console.log('*** app end ***');
+
+ console.log('');
+
